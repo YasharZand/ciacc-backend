f0a8abfd2ef2f180adfd54ff24632f22d1c5b254
“Yashar <79.zand@gmail.com>
2020-07-13 01:46:34 +0800

    deploying on netlify

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(80);
-  //for development : await app.listen(3000);
+  await app.listen(3000);
 }
 bootstrap();
